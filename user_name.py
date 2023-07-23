83b4abb2b9bb69af314cdbcfc23b34d7fdbd0964
roopesh-kumar-sharma <roopesharma1335@gmail.com>
2023-07-23 21:50:57 +0545

    Added 4 files

---
+++ user_name.py
@@ -0,0 +1,15 @@
+#This is  sample code for git practise
+users=[]
+print("Welcome To the College")
+user_continue='yes'
+
+name=input("Enter your name").lower
+print("Welcome to the College",name
+users=users.append(name)
+	
+if user_continue=='no':
+print("Thanks for the visit")
+print(users)
+
+
+
